2b7e0924ec05b1cd1a4f88c03b17b97122624565
Kyle Luke <th.oldschool@gmail.com>
2019-08-07 17:56:00 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,33 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  org_data = {}
+  data.each { |attribute, category|
+    category.each {|subcat, arrays|
+      arrays.each {|name|
+        if org_data[name].nil?
+          org_data[name] = {}
+          org_data[name][attribute] = []
+        else
+          org_data[name][attribute] = []
+        end
+      }
+    }
+  }
+  
+  org_data.each { |names, categories|
+    categories.each {|subcategories, array|
+      data.each { |attribute, category|
+        category.each { |subcat, arrays|
+          arrays.each { |name|
+            
+            if name == names && subcategories == attribute
+              org_data[names][subcategories].push(subcat.to_s)
+            end
+            
+          }
+        }
+      }
+    }
+  }
+  
+  p org_data
 end
